d317cff924ee918782c57fe27e7aaf2939b81a7c
MichaelRoberts214 <m_roberts6@u.pacific.edu>
2017-06-05 17:08:32 -0700

    implement image decoding

--- src.js
+++ src.js
@@ -1,1 +1,11 @@
-window.onload = function() {};
+var decodeImage = function () {
+  var str = document.getElementById('img-input').value;
+  var image = new Image();
+  image.src = str;
+  document.getElementById('image-home').appendChild(image);
+};
+
+window.onload = function() {
+  document.getElementById('decode-button')
+    .addEventListener('click', decodeImage);
+};
